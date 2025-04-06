8bd78e2adc13245ad1492d092d7e6ba84d4d6a7a
Adib <adibpathan280@gmail.com>
2025-04-06 15:45:52 +0930

    boilerplate

--- trader.py
+++ trader.py
@@ -1,68 +1,65 @@
-from typing import Dict, List
-from datamodel import OrderDepth, TradingState, Order
+from datamodel import TradingState, OrderDepth, Order
+from typing import List, Dict
+import string
 
+def run(self, state: TradingState) -> Dict[str, List[Order]]:
+    """
+    Only method required. It takes all buy and sell orders for all symbols as an input,
+    and outputs a list of orders to be sent
+    """
+    # Initialize the method output dict as an empty dict
+    result = {}
 
-class Trader:
+    # Iterate over all the keys (the available products) contained in the order dephts
+    for product in state.order_depths.keys():
 
-    def run(self, state: TradingState) -> Dict[str, List[Order]]:
-        """
-        Only method required. It takes all buy and sell orders for all symbols as an input,
-        and outputs a list of orders to be sent
-        """
-        # Initialize the method output dict as an empty dict
-        result = {}
+        # Retrieve the Order Depth containing all the market BUY and SELL orders
+        order_depth: OrderDepth = state.order_depths[product]
 
-        # Iterate over all the keys (the available products) contained in the order dephts
-        for product in state.order_depths.keys():
+        # Initialize the list of Orders to be sent as an empty list
+        orders: list[Order] = []
 
-                # Retrieve the Order Depth containing all the market BUY and SELL orders
-                order_depth: OrderDepth = state.order_depths[product]
+        # Note that this value of 1 is just a dummy value, you should likely change it!
+        acceptable_price = 10
 
-                # Initialize the list of Orders to be sent as an empty list
-                orders: list[Order] = []
+        # If statement checks if there are any SELL orders in the market
+        if len(order_depth.sell_orders) > 0:
 
-                # Note that this value of 1 is just a dummy value, you should likely change it!
-                acceptable_price = 10
+            # Sort all the available sell orders by their price,
+            # and select only the sell order with the lowest price
+            best_ask = min(order_depth.sell_orders.keys())
+            best_ask_volume = order_depth.sell_orders[best_ask]
 
-                # If statement checks if there are any SELL orders in the market
-                if len(order_depth.sell_orders) > 0:
+            # Check if the lowest ask (sell order) is lower than the above defined fair value
+            if best_ask < acceptable_price:
+                # In case the lowest ask is lower than our fair value,
+                # This presents an opportunity for us to buy cheaply
+                # The code below therefore sends a BUY order at the price level of the ask,
+                # with the same quantity
+                # We expect this order to trade with the sell order
+                print("BUY", str(-best_ask_volume) + "x", best_ask)
+                orders.append(Order(product, best_ask, -best_ask_volume))
 
-                    # Sort all the available sell orders by their price,
-                    # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
+        # The below code block is similar to the one above,
+        # the difference is that it find the highest bid (buy order)
+        # If the price of the order is higher than the fair value
+        # This is an opportunity to sell at a premium
+        if len(order_depth.buy_orders) != 0:
+            best_bid = max(order_depth.buy_orders.keys())
+            best_bid_volume = order_depth.buy_orders[best_bid]
+            if best_bid > acceptable_price:
+                print("SELL", str(best_bid_volume) + "x", best_bid)
+                orders.append(Order(product, best_bid, -best_bid_volume))
 
-                    # Check if the lowest ask (sell order) is lower than the above defined fair value
-                    if best_ask < acceptable_price:
+        # Add all the above the orders to the result dict
+        result[product] = orders
 
-                        # In case the lowest ask is lower than our fair value,
-                        # This presents an opportunity for us to buy cheaply
-                        # The code below therefore sends a BUY order at the price level of the ask,
-                        # with the same quantity
-                        # We expect this order to trade with the sell order
-                        print("BUY", str(-best_ask_volume) + "x", best_ask)
-                        orders.append(Order(product, best_ask, -best_ask_volume))
+    traderData = "SAMPLE"  # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
 
-                # The below code block is similar to the one above,
-                # the difference is that it find the highest bid (buy order)
-                # If the price of the order is higher than the fair value
-                # This is an opportunity to sell at a premium
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        print("SELL", str(best_bid_volume) + "x", best_bid)
-                        orders.append(Order(product, best_bid, -best_bid_volume))
+    conversions = 1
 
-                # Add all the above the orders to the result dict
-                result[product] = orders
-                
-        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
-        
-        conversions = 1 
+    # Return the dict of orders
+    # These possibly contain buy or sell orders
+    # Depending on the logic above
 
-                # Return the dict of orders
-                # These possibly contain buy or sell orders
-                # Depending on the logic above
-        
-        return result, conversions, traderData
+    return result, conversions, traderData
